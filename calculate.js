eef3f868015efac7b7971e632978ca29db9e4a1c
Satyam <satyamaharyan@gmail.com>
2022-11-15 03:21:18 +0530

    hero

--- calculate.js
+++ calculate.js
@@ -15,6 +15,7 @@ for(let i=0;i<list.length-1;i++)
 list[list.length-1].addEventListener('click',function evaluate(){
     val=ans;
     document.getElementById('screen').textContent=eval(val);
+     ans=eval(val);
 });
 
 const endlist=document.getElementsByClassName('end');
@@ -25,6 +26,8 @@ endlist[0].addEventListener('click',function end()
     
 });
 endlist[1].addEventListener('click',function back(){
+     ans=ans.toString();
     newstr=ans.slice(0,ans.length-1);
     document.getElementById('screen').textContent=newstr;
+    ans=newstr;
 });
